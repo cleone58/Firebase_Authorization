d3cc6d448883b6608553ab8e23369901d376108d
Cleone Jacob <therealcleonejacob@gmail.com>
2017-03-15 15:50:23 -0400

    Log Out Button created

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
@@ -25,12 +25,24 @@ class App extends Component {
 
   }
 
+  renderContent() {
+    if (this.state.loggedIn) {
+      return (
+        <Button>
+          Log Out
+        </Button>
+      );
+    }
+
+    return <LoginForm />;
+  }
+
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
